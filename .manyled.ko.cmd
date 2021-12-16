ce81f2e5e1e60dbc4c9255afff2063c931b630cb
S-Reo <Csrengin115it@gmail.com>
2021-12-16 13:31:15 +0900

    makeで作られるファイルを削除

--- .manyled.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/manyled/manyled.ko := ld -r  -EL  -maarch64elf  --build-id  -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds -o /home/ubuntu/manyled/manyled.ko /home/ubuntu/manyled/manyled.o /home/ubuntu/manyled/manyled.mod.o;  true
--- .manyled.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/manyled/manyled.mod := { echo  /home/ubuntu/manyled/manyled.o;  echo; } > /home/ubuntu/manyled/manyled.mod
